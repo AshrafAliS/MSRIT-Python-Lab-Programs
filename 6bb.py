ddd7df1757314b62eecb58a864f1c7ef2329a9a0
Ashraf Ali <fashionzone004@gmail.com>
2018-12-26 00:21:00 +0530

    Create 6bb.py

---
+++ 6bb.py
@@ -0,0 +1,26 @@
+#(b)	Write a python program to calculate the area of the circle and catch appropriate user defined exception.(Hint: check for invalid radius)
+class Error(Exception):
+   """Base class for other exceptions"""
+   pass
+class ValueTooSmallError(Error):
+   """Raised when the input value is too small"""
+   pass
+class ValueTooLargeError(Error):
+   """Raised when the input value is too large"""
+   pass
+number = 10
+while True:
+   try:
+       i_num = int(input("Enter a number: "))
+       if i_num < number:
+           raise ValueTooSmallError
+       elif i_num > number:
+           raise ValueTooLargeError
+       break
+   except ValueTooSmallError:
+       print("This value is too small, try again!")
+       #print()
+   except ValueTooLargeError:
+       print("This value is too large, try again!")
+       print()
+print("Congratulations! You guessed it correctly.")
